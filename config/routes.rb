c26df0e5ddbb5871b4b33f77c68500637eeb5b3a
aizawad888 <201208476+aizawad888@users.noreply.github.com>
2025-10-02 21:51:07 +0900

    以前のレシピコントローラを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :ingredients, only: [:index, :show]
-  get "recipes/search", to: "recipes#search"
+  get "recipes/search", to: "recipes#search", as: "search_recipes"
   root "ingredients#index"
 end
